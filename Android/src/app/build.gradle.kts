0f5142e67e662c79f995961d1be26e793eaff787
Jing Jin <8752427+jinjingforever@users.noreply.github.com>
2025-05-18 17:28:34 -0700

    Bump version

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     minSdk = 26
     targetSdk = 35
     versionCode = 1
-    versionName = "0.9.0"
+    versionName = "0.9.1"
 
     // Needed for HuggingFace auth workflows.
     manifestPlaceholders["appAuthRedirectScheme"] = "com.google.aiedge.gallery.oauth"
